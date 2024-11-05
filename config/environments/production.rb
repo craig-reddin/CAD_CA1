89a3f6c211f969a430036efac8036060f8efad24
Craig Reddin <craig-reddin@hotmail.com>
2024-11-05 16:20:48 +0000

    CAD_CICD_VERSION27

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Rails.application.configure do
 
   # config.public_file_server.enabled = false
   config.hosts << "54.83.132.170"
-
+  
+  #e750444e91cd4dcdaf892f94a8c6b527f048300fe2334476bc77fc2fef1bcd1f
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
